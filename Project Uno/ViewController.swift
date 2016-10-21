972c08ac8c58b1b4093310a6142daf35cb151f07
Michael marriott <michaelmarriott@michaels-macbook-air.local>
2016-10-21 08:39:19 -0600

    Initial Commit

---
+++ Project Uno/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project Uno
+//
+//  Created by Michael marriott on 2016-10-21.
+//  Copyright © 2016 Michael marriott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
